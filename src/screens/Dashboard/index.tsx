93f4d83a49ce9660ae4d1a070bb884af81a457cb
Igor Lamoia Queiroz <igorlamoia@hotmail.com>
2021-12-01 23:02:56 -0300

    Transaction cards, iphone-x-helper

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
-import { Container, Header, UserInfo, Photo, User, UserGreeting, UserName, UserWrapper, Icon } from './styles';
+import { HighlightCard } from '../../components/HighlightCard';
+import TransactionCard from '../../components/TransactionCard';
+import {
+	Container,
+	Header,
+	UserInfo,
+	Photo,
+	User,
+	UserGreeting,
+	UserName,
+	UserWrapper,
+	Icon,
+	HighlightCards,
+	Transactions,
+	Title,
+} from './styles';
 
-export function Dashboard() {
+export const Dashboard: React.FC = () => {
 	return (
 		<Container>
 			<Header>
@@ -16,6 +31,26 @@ export function Dashboard() {
 					<Icon name='power' />
 				</UserWrapper>
 			</Header>
+			<HighlightCards>
+				<HighlightCard
+					type='up'
+					title='Entradas'
+					amount='R$ 17.400,00'
+					lastTransaction='Última entrada dia 13 de abril'
+				/>
+				<HighlightCard
+					type='down'
+					title='Saídas'
+					amount='R$ 1.259,00'
+					lastTransaction='Última entrada dia 03 de abril'
+				/>
+				<HighlightCard type='total' title='Total' amount='R$ 16.141,00' lastTransaction='01 à 16 de abril' />
+			</HighlightCards>
+
+			<Transactions>
+				<Title>Listagem</Title>
+				<TransactionCard />
+			</Transactions>
 		</Container>
 	);
-}
+};
